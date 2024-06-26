416844fc7cc5db58a995db4a241781a37d8e7c5b
stelianrosca618 <stelianrosca618@outlook.com>
2024-06-27 00:51:23 +0300

    restore

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -15,7 +15,7 @@ const config = require('config.json');
 const httServer = http.createServer(app);
 
 const io = socketio(httServer);
-io.origins('*:*');
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
