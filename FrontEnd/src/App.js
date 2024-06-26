dd74eb835ca9b6f6d1b35c060db5aeebaa2c315f
stelianrosca618 <stelianrosca618@outlook.com>
2024-06-26 16:38:48 +0300

    package update

--- FrontEnd/src/App.js
+++ FrontEnd/src/App.js
@@ -8,7 +8,6 @@ import Dashboard from "./scenes/dashboard";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
 import Form from "./scenes/form";
-import Calendar from "./scenes/calendar";
 import FAQ from "./scenes/faq";
 import Bar from "./scenes/bar";
 import Pie from "./scenes/Pie";
@@ -32,7 +31,6 @@ function App() {
             <Route path="/contacts" element={<Contacts />} />
             <Route path="/invoices" element={<Invoices />} />
             <Route path="/form" element={<Form />} />
-            <Route path="/calendar" element={<Calendar />} />
             <Route path="/faq" element={<FAQ />} />
             <Route path="/bar" element={<Bar />} />
             <Route path="/pie" element={<Pie />} />
