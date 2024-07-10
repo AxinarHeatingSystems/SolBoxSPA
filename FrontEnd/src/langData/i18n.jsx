94068d21b0b30abc91899364c9d54c5f3f9cd00b
AxinarHeatingSystems <115953139+AxinarHeatingSystems@users.noreply.github.com>
2024-07-10 10:46:30 +0300

    Update i18n.jsx
    Added Greek strings file import

--- FrontEnd/src/langData/i18n.jsx
+++ FrontEnd/src/langData/i18n.jsx
@@ -1,6 +1,7 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import { En_Str } from "./en_str";
+import { Gr_Str } from "./gr_str";
 import { Es_Str } from "./es_str";
 import { It_Str } from "./it_str";
 
@@ -13,6 +14,9 @@ i18n.use(initReactI18next).init({
     resources: {
         en: {
             translation: En_Str,
+        },
+        gr: {
+            translation: Gr_Str,
         },
         es: {
             translation: Es_Str,
