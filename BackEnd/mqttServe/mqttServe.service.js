cc3b5539ac1aaa7da3b2d9c7b90d0f0ffc04f8e8
stelianrosca618 <stelianrosca618@outlook.com>
2024-07-17 21:53:05 +0300

    update log

--- BackEnd/mqttServe/mqttServe.service.js
+++ BackEnd/mqttServe/mqttServe.service.js
@@ -85,6 +85,8 @@ async function mqttremoveshareduser(devData){
     try {
         await kcAdminClient.users.delFromGroup({id: devData.userId, groupId: devData.devId, realm: config.keycloakRealm});
         const sharedlist = await kcAdminClient.groups.listMembers({id: devData.id, realm: config.keycloakRealm});
+        console.log(sharedlist);
+        console.log('devList')
         resultData = {state: 'success', data: sharedlist};
     } catch (error) {
         resultData = {state: 'failed', message: 'Removing shared user is failed'};  
