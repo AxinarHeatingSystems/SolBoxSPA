5f8974c92bba4f11ecdb4bedf892bbaf1b647794
stelianrosca618 <stelianrosca618@outlook.com>
2024-07-26 22:12:52 +0300

    mqtt connection

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -48,8 +48,8 @@ const options = {
   
   // for more options and details, please refer to https://github.com/mqttjs/MQTT.js#mqttclientstreambuilder-options
 }
-// const mqttPath = `${config.protocol}://${config.host}:${config.port}`
-const mqttPath = `${config.protocol}://172.21.0.6:1883`
+const mqttPath = `${config.protocol}://${config.host}:${config.port}`
+// const mqttPath = `${config.protocol}://172.21.0.6:1883`
 
 console.log(mqttPath);
 const client = mqtt.connect(mqttPath, options);
