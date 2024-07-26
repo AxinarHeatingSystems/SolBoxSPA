098c176418bcead94fe2afdac4defd49d153f971
stelianrosca618 <stelianrosca618@outlook.com>
2024-07-26 18:31:34 +0300

    mqtt socket memory

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -51,7 +51,7 @@ const options = {
 const mqttPath = `${config.protocol}://${config.host}:${config.port}`
 
 const client = mqtt.connect(mqttPath, options);
-client.setMaxListeners(0);
+client.setMaxListeners(1000);
 client.on('connect', () => {
   console.log(`${config.protocol}: Connected`);
 })
