0876cd7b2d80bb22afa38c86a613e7c84d296279
stelianrosca618 <stelianrosca618@outlook.com>
2024-07-26 22:36:00 +0300

    update config.json file

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -48,8 +48,8 @@ const options = {
   
   // for more options and details, please refer to https://github.com/mqttjs/MQTT.js#mqttclientstreambuilder-options
 }
-// const mqttPath = `${config.protocol}://${config.host}:${config.port}`
-const mqttPath = `${config.protocol}://EMQX:1883`
+const mqttPath = `${config.protocol}://${config.host}:${config.port}`
+// const mqttPath = `${config.protocol}://EMQX:1883`
 
 console.log(mqttPath);
 const client = mqtt.connect(mqttPath, options);
